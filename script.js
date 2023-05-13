98daf5bb9e2547c924e3f189eb5cfb1033c264b0
aidenburrell <131427182+aidenburrell@users.noreply.github.com>
2023-05-13 03:02:10 +0000

    Changed a number of things

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 
 printPyramid(5);
-// TODO #2
-// Take in user input for the height
 
-
+function printPyramid(height) {
+  for (let row = 0; row < height; row++) {
+    let bricks = "";
+    let spaces = "";
+    for (let col = 0; col < height - row - 1; col++) {
+      spaces += " ";
+    }
+    for (let col = 0; col < row + 1; col++) {
+      bricks += "#";
+    }
+    console.log(spaces + bricks);
+  }
+}
 /*
  * printPyramid
  *
@@ -19,7 +29,25 @@ function printPyramid(height) {
     console.log("Uh oh... the pyramid is under construction.");
     console.log("Check back soon, our developers are hard at work as we speak!");
 
-    // TODO #1
-    // print that pyramid!
+ 
+const container = document.getElementById("pyramid");
+const rowElement = document.createElement("p");
+rowElement.innerText = rowStr;
+container.appendChild(rowElement);
+
+const notice = document.getElementById("notice");
+notice.remove();
+
+}
+const pyramidButton = document.getElementById("pyramidButton");
+
+pyramidButton.addEventListener("click", function() {
+  const height = document.getElementById("height").value;
+  determineHeightAndThenDrawPyramid(height);
+});
+
+function drawPyramid(height) {
+
+  document.getElementById("pyramid").innerHTML = "";
 
 }
